8650b4b7c558d1a95453cfa68b1afc6b718231ed
Lilirose200x <48535851+Lilirose200x@users.noreply.github.com>
2022-10-06 19:19:19 -0400

    Add files via upload

---
+++ Library-Frontend/src/views/Librarian_Function/LibrarianUser.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div>
+      <!--      Setting of green button in the top of the page-->
+      <b-form inline>
+        <b-button v-on:click="signUpPage" variant="outline-success">Create a new account for a user</b-button>
+      </b-form>
+    </div>
+    <!--    Setting of bootstrap table displayed the users' information-->
+    <div class="table" style="overflow: auto;height: 51vh;">
+      <b-table style="margin-top: 40px;" hover :items="userDisplayList"
+               @row-selected="onRowSelected"
+               select-mode="single"
+               selectable>
+      </b-table>
+    </div>
+
+    <div style="display: flex;justify-content: flex-end;">
+      <b-pagination
+        v-model="currentPage"
+        :total-rows="rows"
+        :per-page="perPage"
+        aria-controls="my-table"
+      ></b-pagination>
+    </div>
+    <!--Setting of buttons in the bottom of the page-->
+    <div class="btns" style="text-align: right;border-top: #e7e7e7 1px solid;padding-top: 20px">
+      <b-button variant="outline-primary" @click="handleCancel">Close</b-button>
+      <b-button @click="deleteUser(selectedUser)" variant="primary">Delete</b-button>
+    </div>
+
+  </div>
+</template>
+
+<script src="./LibrarianUser_JS.js">
+</script>
+
+<style scoped>
+</style>
---
+++ Library-Frontend/src/views/Start/Start.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="Start">
+    <div class="StartMenu">
+      Welcome to Group 18 Library!
+    </div>
+    <b-form-group style="height: 60vh ">
+      <!--      Button of User-->
+      <div style="text-align: center">
+        <div @click="gotoUser()" class="goToUser">
+          User
+        </div>
+      </div>
+      <!--      Button of Librarian-->
+      <div style="text-align: center">
+        <div @click="gotoLibrarian()" class="goToLibrarian">
+          Librarian
+        </div>
+      </div>
+    </b-form-group>
+    <div style="padding-top: 175px"></div>
+  </div>
+</template>
+<script>
+
+export default {
+  methods: {
+    /**
+     * Goes to user start page
+     */
+    gotoUser() {
+      this.$router.push('/userStart');
+    },
+    /**
+     * Goes to librarian login page
+     */
+    gotoLibrarian() {
+      this.$router.push('/LibrarianMenu');
+    }
+  }
+
+}
+
+</script>
+<style>
+.Start {
+  background-image: url("../image/Library_BookShelves.jpeg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: scroll;
+}
+
+.StartMenu {
+  font-size: 32px;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  box-shadow: 0 0 50px rgba(0, 0, 0, 0.3);
+  padding: 10px;
+  background: #fff;
+}
+
+.goToUser {
+  height: 120px;
+  padding: 2% 0 5% 0;
+  margin: 5% 40%;
+  border-radius: 15px;
+  font-size: 40px;
+  color: white;
+  background-color: dodgerblue;
+  cursor: pointer;
+}
+
+.goToLibrarian {
+  height: 120px;
+  padding: 2% 0 5% 0;
+  margin: 5% 40%;
+  border-radius: 15px;
+  font-size: 40px;
+  color: white;
+  background-color: dodgerblue;
+  cursor: pointer;
+}
+
+.goToUser:hover {
+  background-color: #0000ff;
+}
+
+.goToLibrarian:hover {
+  background-color: #0000ff;
+}
+</style>
